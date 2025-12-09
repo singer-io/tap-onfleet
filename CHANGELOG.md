9f38c8ea6fc7d27357d8c0932063be1ff6ca7948
Bryant Gray <bryant.gray@qlik.com>
2025-12-09 12:13:47 -0500

    bump dependencies (#10)
    * bump deps
    
    * bump version and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 1.0.2
+  * Bump dependencies [#10](https://github.com/singer-io/tap-onfleet/pull/10)
+
 ## 1.0.1
   * Dependabot update [#6](https://github.com/singer-io/tap-onfleet/pull/6)
 
--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup
 
 setup(name='tap-onfleet',
-      version='1.0.1',
+      version='1.0.2',
       description='Singer.io tap for extracting data from the onfleet API',
       author='Stitch',
       url='http://github.com/lambtron/tap-onfleet',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_onfleet'],
       install_requires=[
-          'singer-python==5.1.5',
-          'requests==2.31.0',
-          'backoff==1.3.2'
+          'singer-python==5.14.3',
+          'requests==2.32.5',
+          'backoff==1.10.0'
       ],
       entry_points='''
           [console_scripts]
