e4cdd4d5048145bf7cdaaf4c46a6ac1373cf3856
akkumar-qlik <akarsh.kumar@qlik.com>
2026-05-11 10:31:14 +0530

    Added updated library version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-onfleet',
       py_modules=['tap_onfleet'],
       install_requires=[
           'singer-python==6.8.0',
-          'requests==2.32.5',
+          'requests==2.33.1',
           'backoff==2.2.1',
       ],
       extras_require={
